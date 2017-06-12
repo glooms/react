de560363a93d63d9046f48ec1546e7e761184b0c
glooms <marcus.lacerda@live.se>
2017-06-12 10:20:28 +0200

    Clean starting branch for projects.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import ProductListing from './ProductListing';
-import Cart from './Cart';
+
 
 class Layout extends React.Component
 {
     constructor() {
         super();
-        this.buyProduct = this.buyProduct.bind(this);
     }
 
     render()
     {
         return (
             <div>
-                <h1>Köp för fan!</h1>
-                <Cart />
-                <ProductListing />
+                Hej.
             </div>
         );
     }
-    
-   buyProduct()
-   {
-       this.setState({}); 
-   } 
 }
 
 export default Layout;
--- src/js/store.js
+++
@@ -1,1 +0,0 @@
-export default {addProduct: null};
